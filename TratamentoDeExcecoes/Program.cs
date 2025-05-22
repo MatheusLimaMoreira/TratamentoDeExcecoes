8259c7b87a1452e05e5ab4efc6fdf98c0b037293
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-22 12:05:17 -0300

    Inclusao try e catch

---
+++ TratamentoDeExcecoes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TratamentoDeExcecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Exceção de divisão por zero
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int resultado = n1 / n2;
+                Console.WriteLine(resultado);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Error! " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format Error! " + e.Message);
+            }            
+        }   
+    }
+}
